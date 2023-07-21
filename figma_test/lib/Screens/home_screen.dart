beaadea4f3ae2129aff7a344e5b7619d0575f6c1
nabeeeltk <nabeelmuhammed406@gmail.com>
2023-07-21 12:10:13 +0530

    completed

--- figma_test/lib/Screens/home_screen.dart
+++ figma_test/lib/Screens/home_screen.dart
@@ -1,37 +1,79 @@
-
 import 'package:figma_test/widgets/home_grid_view.dart';
 import 'package:figma_test/widgets/search_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  HomeScreen({Key? key}) : super(key: key);
+
+  final List<String> date = [
+    "*\n26",
+    "金\n27",
+    "土\n28",
+    "日\n29",
+    "月\n30",
+    "火\n01",
+    "水\n02",
+  ];
+
+  final List<Color> containercolor = [
+    Colors.orange,
+    Colors.white,
+    Colors.white,
+    Colors.white,
+    Colors.white,
+    Colors.white,
+    Colors.white,
+    Colors.white,
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-         child: Icon(Icons.place,size:40 ,),
-        backgroundColor: Colors.white,
-        onPressed: (){}),
+          child: Icon(
+            Icons.place,
+            size: 40,
+          ),
+          backgroundColor: Colors.white,
+          onPressed: () {}),
       body: Column(
         children: [
-         const  SearchBar(), // Assuming this is a custom widget for the search bar
+          const SearchBar(), // Assuming this is a custom widget for the search bar
           Container(
             height: 40,
             width: double.infinity,
-            decoration:const  BoxDecoration(
-              gradient: LinearGradient(colors: [
-                Colors.amber,
-                Colors.orange
-              ])
-            ),
-            child: const Center(child: Text("2022年 5月 26日（木）")
-            ),
+            decoration: const BoxDecoration(
+                gradient:
+                    LinearGradient(colors: [Colors.amber, Colors.orange])
+                    
+                    
+                    ),
+            child: const Center(child: Text("2022年 5月 26日（木）")),
           ),
-         const  SizedBox(height: 20,),
-         const Expanded(child: 
-             HomeGridView()
-         ),
+
+          SizedBox(
+            height: 80,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: date.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 50,
+                    width: 50,
+                    margin: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                       color: containercolor[index % containercolor.length],
+                        borderRadius: BorderRadius.circular(10)
+                        ),
+                    child: Center(
+                        child: Text(
+                      date[index],
+                      style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                    )),
+                  );
+                }),
+          ),
+          const Expanded(child: HomeGridView()),
         ],
       ),
     );
--- figma_test/lib/widgets/stamp_container.dart
+++ figma_test/lib/widgets/stamp_container.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+
 
 class StampContainer extends StatelessWidget {
   const StampContainer({super.key});
@@ -9,6 +8,7 @@ class StampContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 200,
+      width: double.infinity,
     
       child: PageView.builder(
         itemCount: 3,
@@ -17,11 +17,11 @@ class StampContainer extends StatelessWidget {
           return Container(
             height: 200,
             width: double.infinity,
-            margin: EdgeInsets.only(right: 10),
+            margin:const  EdgeInsets.only(right: 10),
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(20),
-                image: DecorationImage(
-                    image: AssetImage("image/doctor3.jpg"), fit: BoxFit.cover)),
+                image:const  DecorationImage(
+                    image: AssetImage("image/yellowstar.jpg"), fit: BoxFit.cover)),
           );
         
         },
